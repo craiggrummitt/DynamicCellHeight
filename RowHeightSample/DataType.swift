8b18a527d45361ace204465fc9a78d859c370c68
craiggrummitt <craiggrummitt@gmail.com>
2014-10-27 14:19:39 -0500

    First Commit

---
+++ RowHeightSample/DataType.swift
@@ -0,0 +1,18 @@
+//
+//  DataType.swift
+//  RowHeightSample
+//
+//  Created by Craig Grummitt on 10/27/14.
+//  Copyright (c) 2014 Craig Grummitt. All rights reserved.
+//
+
+import UIKit
+
+class DataType {
+    var text:String
+    var height:CGFloat
+    init(text:String,height:CGFloat) {
+        self.text=text
+        self.height=height
+    }
+}
